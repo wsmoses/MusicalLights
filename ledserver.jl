d084a20866234048a1df3e7a6a279914fff90f16
Joseph Murphy <air.jmurph@gmail.com>
2017-05-18 06:18:28 -0400

    Changed over from TCP to UDP

--- ledserver.jl
+++ ledserver.jl
@@ -93,7 +93,7 @@ function main()
         =#
         @async begin
          udpsock = UDPSocket()
-         bind(udpsock,ip"127.0.0.1",8080)
+         bind(udpsock,ip"10.42.0.109",8080)
          while true
            parseAndUpdate(bytestring(recv(udpsock)))
          end
