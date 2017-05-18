48ba2f118d9f9b12ef879f952116f5d1746e3dc8
Joseph Murphy <air.jmurph@gmail.com>
2017-05-18 05:41:14 -0400

    Added a little debugging code

--- ledserver.jl
+++ ledserver.jl
@@ -1,10 +1,21 @@
 using PyCall;
 @pyimport neopixel
+LED_COUNT      = 16      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = true    # True to invert the signal (when using NPN transistor level shift)
 function num2byteA{T<:Union{Float16, Float32, Float64, Signed, Unsigned}}(x::T)
    iob = IOBuffer()
    write(iob, x)
    seekstart(iob)
    return readbytes(iob)
+end
+function clearLights(ledstrip)
+    for i in 1:LED_COUNT
+        setColorRGB(ledstrip, i, 0,0,0)
+    end
 end
 function serverInfo()
     #=
@@ -60,12 +71,6 @@ function setColorRGB(ledstrip, i, R, G, B)
 end
 function openHardware()
     # LED strip configuration:
-    LED_COUNT      = 16      # Number of LED pixels.
-    LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
-    LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-    LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
-    LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
-    LED_INVERT     = true    # True to invert the signal (when using NPN transistor level shift)
     ledstrip = neopixel.Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
     ledstrip[:begin]()
     return ledstrip
@@ -73,6 +78,7 @@ end
 function main()
     server = listen(IPv4(0), 8080)
     ledstrip = openHardware()
+    clearLights(ledstrip)
     try
         while true
             socket = accept(server)
@@ -89,5 +95,6 @@ function main()
         end
     end
 end
+
 println("Server Listening on port 8080, use <Ctrl>-C to end the program.")
 main()
